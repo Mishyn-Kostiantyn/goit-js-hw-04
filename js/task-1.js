53ae3ef9bf2925d2e92373ef6ff45059b6c67424
Mishyn-Kostiantyn <129659279+Mishyn-Kostiantyn@users.noreply.github.com>
2023-11-13 15:35:09 +0200

    Виконав ДЗ.  Модуль 4

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,24 @@
-function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+function isEnoughCapacity(products, containerSize) {
+    let totalQuantity = 0;
+    let y = Object.values(products);
+    for (let x = 0; x < y.length; x++) { totalQuantity += y[x] };
+    if (totalQuantity > containerSize) { return false; } else { return true; }
 }
-console.log(makeTransaction(5, 3000)); 
-console.log(makeTransaction(3, 1000)); 
-console.log(makeTransaction(10, 500)); 
-console.log(makeTransaction(9, 5));
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 25)
+); 
